a5b463659acdd65767391c9cead5a1ea60da0ed5
Plib <Tomas.Wynne@gmail.com>
2026-05-24 01:21:45 +0000

    Added MLFlow to py files

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pickle
 import mlflow
-import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
@@ -9,49 +8,39 @@ from sklearn.metrics import r2_score, mean_squared_error
 PROCESSED_DATA_PATH = "data/processed/processed.csv"
 MODEL_PATH = "models/model.pkl"
 
-# For testing in Github Actions
-TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns")
-mlflow.set_tracking_uri(TRACKING_URI)
+mlflow.set_tracking_uri("http://34.39.99.72:5555")
+mlflow.set_experiment("medical-charges-regression")
 
 
-def evaluate_model():
-    # Load processed dataset
-    df = pd.read_csv(PROCESSED_DATA_PATH)
+# Load processed dataset
+df = pd.read_csv(PROCESSED_DATA_PATH)
 
-    # Features and target
-    X = df.drop("charges", axis=1)
-    y = df["charges"]
+# Features and target
+X = df.drop("charges", axis=1)
+y = df["charges"]
 
-    # Train/Test Split
-    _, X_test, _, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42
-    )
+# Train/Test Split
+_, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Load trained model
-    with open(MODEL_PATH, "rb") as file:
-        model = pickle.load(file)
+# Load trained model
+with open(MODEL_PATH, "rb") as file:
+    model = pickle.load(file)
 
-    # Predictions
-    predictions = model.predict(X_test)
+# Predictions
+predictions = model.predict(X_test)
 
-    # Metrics
-    r2 = r2_score(y_test, predictions)
-    mse = mean_squared_error(y_test, predictions)
+# Metrics
+r2 = r2_score(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
 
-    print(f"\nR2 Score: {r2}")
-    print(f"Mean Squared Error: {mse}")
+# CI fails if model is bad
+if r2 < 0.7:
+    raise Exception("Model rejected: performance too low")
 
-    # Log metrics to MLflow
-    with mlflow.start_run():
+print(f"\nR2 Score: {r2}")
+print(f"Mean Squared Error: {mse}")
 
-        mlflow.log_metric("r2_score", r2)
-        mlflow.log_metric("mse", mse)
-
-    return r2, mse
-
-
-if __name__ == "__main__":
-    evaluate_model()
+# Log metrics to MLflow
+with mlflow.start_run():
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("mse", mse)
--- src/train.py
+++ src/train.py
@@ -6,65 +6,69 @@ import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
 
 PROCESSED_DATA_PATH = "data/processed/processed.csv"
 MODEL_OUTPUT_PATH = "models/model.pkl"
 
-# For testing in Github Actions
-TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns")
-mlflow.set_tracking_uri(TRACKING_URI)
+mlflow.set_tracking_uri("http://34.39.99.72:5555")
+
+# Create experiment
+mlflow.set_experiment("medical-charges-regression")
+
+# Load processed dataset
+df = pd.read_csv(PROCESSED_DATA_PATH)
+
+# Features and target
+X = df.drop("charges", axis=1)
+y = df["charges"]
+
+print("\nFeature Columns:")
+print(X.columns)
+
+print("\nTarget Column:")
+print(y.name)
+
+# Train/Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Start MLflow run
+with mlflow.start_run():
+
+    # Train model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Evaluate
+    predictions = model.predict(X_test)
+    r2 = r2_score(y_test, predictions)
+    mse = mean_squared_error(y_test, predictions)
+
+    # Save model locally
+    os.makedirs(os.path.dirname(MODEL_OUTPUT_PATH), exist_ok=True)
+
+    with open(MODEL_OUTPUT_PATH, "wb") as file:
+        pickle.dump(model, file)
+
+    # Log parameters
+    mlflow.log_param("model_type", "LinearRegression")
+    mlflow.log_param("test_size", 0.2)
+    mlflow.log_param("random_state", 42)
+    
+    # Log metrics
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("mse", mse)
+
+    # Log model artifact
+    result = mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
 
 
-def train_model():
-    # Load processed dataset
-    df = pd.read_csv(PROCESSED_DATA_PATH)
-
-    # Features and target
-    X = df.drop("charges", axis=1)
-    y = df["charges"]
-
-    print("\nFeature Columns:")
-    print(X.columns)
-
-    print("\nTarget Column:")
-    print(y.name)
-
-    # Train/Test Split
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42
+    # Register model
+    mlflow.register_model(
+        model_uri=result.model_uri,
+        name="medical-charges-linear-regression"
     )
 
-    # Start MLflow run
-    with mlflow.start_run():
-
-        # Train model
-        model = LinearRegression()
-        model.fit(X_train, y_train)
-
-        # Save model
-        os.makedirs(os.path.dirname(MODEL_OUTPUT_PATH), exist_ok=True)
-        with open(MODEL_OUTPUT_PATH, "wb") as file:
-            pickle.dump(model, file)
-
-        # Log model to MLflow
-        result = mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model"
-        )
-
-        # Register model
-        mlflow.register_model(
-            model_uri=result.model_uri,
-            name="medical-charges-linear-regression"
-        )
-
-        print(f"\nModel trained and saved to: {MODEL_OUTPUT_PATH}")
-
-    return X_test, y_test
-
-
-if __name__ == "__main__":
-    train_model()
+print(f"\nModel trained and saved to: {MODEL_OUTPUT_PATH}")
+print(f"R2 Score: {r2}")
+print(f"MSE: {mse}")
